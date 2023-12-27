58ddbc4fa667f4bd80aff0714650227f634077b0
Kerry Buckley <kerryjbuckley@gmail.com>
2023-12-27 14:59:32 +0000

    Add failing (non-compiling) test for ‘bid and lose’

--- test/auction_sniper/end_to_end_test.exs
+++ test/auction_sniper/end_to_end_test.exs
@@ -19,5 +19,16 @@ defmodule AuctionSniper.EndToEndTest do
       FakeAuctionServer.announce_closed(auction)
       ApplicationRunner.shows_sniper_has_lost_auction()
     end
+
+    test "sniper makes a higher bid but loses", %{auction: auction} do
+      FakeAuctionServer.start_selling_item(auction)
+      ApplicationRunner.start_bidding_in(auction)
+      FakeAuctionServer.has_received_join_request_from_sniper(auction)
+      FakeAuctionServer.report_price(auction, 1000, 98, "other bidder")
+      FakeAuctionServer.has_received_bid(auction, 1098, ApplicationRunner.sniper_xmpp_id())
+      ApplicationRunner.has_shown_sniper_is_bidding()
+      FakeAuctionServer.announce_closed(auction)
+      ApplicationRunner.shows_sniper_has_lost_auction()
+    end
   end
 end
