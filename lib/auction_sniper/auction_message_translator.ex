8e2f561ec137597838a6fe2aecbbe00edc538516
Kerry Buckley <kerryjbuckley@gmail.com>
2023-12-29 11:52:48 +0000

    Flesh out failing test for AuctionMessageTranslator.process_message/1
    The book includes an unused `chat` argument, but they specifically say
    “as it happens, we don’t need one for the current functionality”, so I’m
    calling Yagni.
    
    Rather than mocking an interface (behaviour), this version is expecting
    a message to be sent to a process. In real life this will be the main
    application server, but for the test it will be to the test process
    itself, allowing us to simply use `assert_receive`.

---
+++ lib/auction_sniper/auction_message_translator.ex
@@ -0,0 +1,6 @@
+defmodule AuctionSniper.AuctionMessageTranslator do
+  @moduledoc false
+
+  def process_message(_message) do
+  end
+end
--- test/auction_sniper/auction_message_translator_test.exs
+++ test/auction_sniper/auction_message_translator_test.exs
@@ -1,6 +1,12 @@
 defmodule AuctionSniper.AuctionMessageTranslatorTest do
   use ExUnit.Case, async: true
 
+  alias AuctionSniper.AuctionMessageTranslator
+  alias Romeo.Stanza.Message
+
   test "notifies auction closed when close message received" do
+    message = %Message{body: "SOLVersion: 1.1; Event: CLOSE;"}
+    AuctionMessageTranslator.process_message(message)
+    assert_receive :auction_closed
   end
 end
