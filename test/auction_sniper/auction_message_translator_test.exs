f3545e878e37a1d0db31b3823f26016f134501ce
Kerry Buckley <kerryjbuckley@gmail.com>
2023-12-29 12:12:37 +0000

    Add failing test for current price event

--- test/auction_sniper/auction_message_translator_test.exs
+++ test/auction_sniper/auction_message_translator_test.exs
@@ -4,6 +4,12 @@ defmodule AuctionSniper.AuctionMessageTranslatorTest do
   alias AuctionSniper.AuctionMessageTranslator
   alias Romeo.Stanza.Message
 
+  test "notifies bid details when current price message received" do
+    message = %Message{body: "SOLVersion: 1.1; Event: PRICE, CurrentPrice: 192, Increment: 7, Bidder: Someone else;"}
+    AuctionMessageTranslator.process_message(message)
+    assert_receive {:current_price, 192, 7}
+  end
+
   test "notifies auction closed when close message received" do
     message = %Message{body: "SOLVersion: 1.1; Event: CLOSE;"}
     AuctionMessageTranslator.process_message(message)
